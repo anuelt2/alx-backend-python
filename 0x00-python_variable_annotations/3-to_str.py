1fa7f12c693884d2fe54021d6008fa7fddc38612
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-04-24 09:41:05 +0000

    Add 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module with to_str function"""
+
+
+def to_str(n: float) -> str:
+    """Type-annotated function that returns string representation of float"""
+    return str(n)
