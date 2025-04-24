ace9da146b80160f69a5fd15fecccea22257b1f3
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-04-24 09:22:40 +0000

    Add 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module with concat function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Type-annotated function that returns concatenated string from arguments
+    """
+    return str1 + str2
