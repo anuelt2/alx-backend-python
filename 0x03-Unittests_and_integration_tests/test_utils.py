49454422cc0cf4717b0bbfa2bfa92087ba615294
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-05-16 11:13:57 +0000

    Add TestGetJson test class and test_get_json method to test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,8 +4,9 @@
 
 from parameterized import parameterized
 import unittest
+from unittest.mock import patch, Mock
 
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -36,3 +37,25 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
         self.assertEqual(str(context.exception), f"'{path[-1]}'")
+
+
+class TestGetJson(unittest.TestCase):
+    """ Unit test class for `get_json` function
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+        ])
+    def test_get_json(self, test_url, test_payload):
+        """
+        Testing that `get_json` function returns the expected payload
+        from URL
+        """
+        with patch("utils.requests.get") as mock_get:
+            mock_response = Mock()
+            mock_response.json.return_value = test_payload
+            mock_get.return_value = mock_response
+
+            response = get_json(test_url)
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(response, test_payload)
