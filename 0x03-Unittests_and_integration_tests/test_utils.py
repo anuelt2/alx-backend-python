bc7717dcdbf5ff3b78c010b3905b710f8c67111a
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-05-19 21:30:53 +0000

    Add test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,24 +5,19 @@
 import unittest
 from parameterized import parameterized
 from unittest.mock import patch, Mock
-
 from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """ Unit test class for `access_nested_map` function
     """
-
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
         ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """
-        Testing that `access_nested_map` function returns expected value
-        when given a nested map with a path of map keys
-        """
+        """Testing that `access_nested_map` function returns expected value"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
